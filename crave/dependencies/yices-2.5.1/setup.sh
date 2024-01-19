4ee3b385245c4c0e26aded475a00c30c9d792549
stas <stas@minres.com>
2024-01-19 12:18:14 +0100

    add Yices2 SMT

--- crave/dependencies/yices-2.5.1/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-version=2.5.1
-
-config_files_dir=$base_dir/yices-2.4.2
-
-source $base_dir/yices-2.4.2/shared.sh
--- crave/dependencies/yices-git/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-version=git
-branch=master
-
-config_files_dir=$base_dir/yices-git
-
-source $base_dir/yices-git/shared.sh
