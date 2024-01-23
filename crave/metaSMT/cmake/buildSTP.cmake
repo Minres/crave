cadebe295f0ca9b96685ac76f8bb8a72a67b06d1
stas <stas@minres.com>
2024-01-23 15:21:03 +0100

    add MiniSat solver

--- crave/metaSMT/cmake/buildSTP.cmake
+++ crave/metaSMT/cmake/buildSTP.cmake
@@ -8,29 +8,22 @@ FetchContent_GetProperties(stp_repo)
 if(NOT stp_repo_POPULATED)
     FetchContent_Populate(stp_repo)
 
+    include(cmake/buildMiniSat.cmake)
+
     # install dir must be availble to configure time otherwise CMake will complain
     set(stp_install_dir ${CMAKE_BINARY_DIR}/_deps/stp-install)
-
     if(NOT stp_EXTERNAL_PROJECT_ADDED)
         execute_process(
             COMMAND mkdir -p ${stp_install_dir}/include
             WORKING_DIRECTORY ${stp_repo_SOURCE_DIR}
         )
 
-        set(minisat_install_dir ${CMAKE_BINARY_DIR}/_deps/minisat_ep-install)
-        ExternalProject_Add(
-            minisat
-            GIT_REPOSITORY https://github.com/stp/minisat.git
-            GIT_TAG releases/2.2.1
-            CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${minisat_install_dir} 
-            )
-
         ExternalProject_Add(
             stp
             BUILD_IN_SOURCE TRUE
             SOURCE_DIR ${stp_repo_SOURCE_DIR}
-            CMAKE_ARGS -DENABLE_PYTHON_INTERFACE=off -DONLY_SIMPLE=on -DNOCRYPTOMINISAT=on -DCMAKE_INSTALL_PREFIX=${stp_install_dir} -DMINISAT_INCLUDE_DIR=${minisat_install_dir}/include -DMINISAT_LIBRARY=${minisat_install_dir}/lib/libminisat.so 
-            DEPENDS minisat
+            CMAKE_ARGS -DENABLE_PYTHON_INTERFACE=off -DONLY_SIMPLE=on -DNOCRYPTOMINISAT=on -DCMAKE_INSTALL_PREFIX=${stp_install_dir} -DMINISAT_INCLUDE_DIR=${MINISAT_INCLUDE_DIRS} -DMINISAT_LIBRARY=${MINISAT_LIBRARY_DIRS}/libminisat.so 
+            DEPENDS minisat_git::minisat_git
         )
 
         set(stp_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
