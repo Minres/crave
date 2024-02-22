be8106552d5edbd423f45088693b1347d2e19b0c
Eyck Jentzsch <eyck@minres.com>
2024-02-22 08:43:28 +0100

    update build instructions for z3 and cudd

--- crave/metaSMT/cmake/buildCUDD.cmake
+++ crave/metaSMT/cmake/buildCUDD.cmake
@@ -7,36 +7,56 @@ FetchContent_GetProperties(cudd_repo)
 
 if(NOT cudd_repo_POPULATED)
     FetchContent_Populate(cudd_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/cudd)
-    endif()
-
-    if(NOT CUDD_EXTERNAL_PROJECT_ADDED)
-        execute_process(
-            COMMAND mkdir -p ${install_dir}/include
-            COMMAND mkdir -p ${install_dir}/lib
-            WORKING_DIRECTORY ${cudd_repo_SOURCE_DIR}
-        )
-
-        ExternalProject_Add(
-            cudd
-            BUILD_IN_SOURCE TRUE
-            SOURCE_DIR ${cudd_repo_SOURCE_DIR}
-            CONFIGURE_COMMAND touch configure.ac aclocal.m4 configure Makefile.am Makefile.in
-            COMMAND ./configure --enable-obj --enable-dddmp --prefix=${install_dir}
-            BUILD_COMMAND make -j
-            INSTALL_COMMAND make install -j
-        )
-
-        set(CUDD_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    add_library(cudd_git::cudd_git INTERFACE IMPORTED)
-    target_include_directories(cudd_git::cudd_git INTERFACE ${install_dir}/include)
-    target_link_libraries(cudd_git::cudd_git INTERFACE cudd)
-    target_link_directories(cudd_git::cudd_git INTERFACE ${install_dir}/lib)
-    message(STATUS "Use CUDD from ${install_dir}")
 endif()
+set(install_dir ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
+    set(install_dir ${CMAKE_BINARY_DIR}/solvers/cudd)
+endif()
+
+ExternalProject_Add(
+    cudd_git
+    BUILD_IN_SOURCE TRUE
+    SOURCE_DIR ${cudd_repo_SOURCE_DIR}
+    CONFIGURE_COMMAND touch configure.ac aclocal.m4 configure Makefile.am Makefile.in
+    COMMAND ./configure --enable-obj --enable-dddmp --prefix=${install_dir}
+    BUILD_COMMAND make -j
+    INSTALL_COMMAND make install -j
+)
+
+add_library(cudd INTERFACE)
+target_include_directories(cudd INTERFACE ${install_dir}/include)
+target_link_libraries(cudd INTERFACE ${install_dir}/lib/libcudd.a)
+target_link_directories(cudd INTERFACE ${install_dir}/lib)
+add_dependencies(cudd cudd_git)
+add_library(cudd::cudd ALIAS cudd)
+
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+set(cudd_CMAKE_CONFIG_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/cudd)
+
+install(TARGETS cudd EXPORT cudd-targets)
+
+install(
+  EXPORT cudd-targets
+  DESTINATION ${cudd_CMAKE_CONFIG_DIR}
+)
+
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/cudd-config-version.cmake
+    VERSION 4.6.0
+    COMPATIBILITY AnyNewerVersion
+)
+
+configure_package_config_file(
+    ${CMAKE_CURRENT_LIST_DIR}/cudd-config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/cudd-config.cmake
+    INSTALL_DESTINATION ${cudd_CMAKE_CONFIG_DIR}
+)
+
+install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/cudd-config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/cudd-config-version.cmake
+    DESTINATION ${cudd_CMAKE_CONFIG_DIR})
+
+message(STATUS "Use CUDD from ${install_dir}")
--- crave/metaSMT/cmake/buildZ3.cmake
+++ crave/metaSMT/cmake/buildZ3.cmake
@@ -4,46 +4,74 @@ FetchContent_Declare(
   GIT_TAG z3-4.6.0
 )
 FetchContent_GetProperties(z3_git)
-
-if(NOT z3_git_POPULATED)
-  FetchContent_Populate(z3_git)
-
-  set(install_dir ${CMAKE_INSTALL_PREFIX})
-  if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-      # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-      set(install_dir ${CMAKE_BINARY_DIR}/solvers/z3)
-  endif()
-
-  if(NOT Z3_EXTERNAL_PROJECT_ADDED)
-    execute_process(
-      COMMAND mkdir -p ${install_dir}/include
-      COMMAND ${CMAKE_COMMAND} -E make_directory ${install_dir}/lib
-      COMMAND git apply -p0 ${CMAKE_CURRENT_LIST_DIR}/z3-z3-4.6.0__permutation_matrix.patch
-      WORKING_DIRECTORY ${z3_git_SOURCE_DIR}
-    )
-
-    ExternalProject_Add(
-      z3
-      BUILD_IN_SOURCE = TRUE
-      SOURCE_DIR ${z3_git_SOURCE_DIR}
-      CONFIGURE_COMMAND ./configure --staticlib --prefix=${install_dir}
-      BUILD_COMMAND make -C build -j
-      INSTALL_COMMAND make -C build install -j
-    )
-    set(Z3_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-  endif()
-
-  add_library(z3_git::z3_git INTERFACE IMPORTED)
-  target_include_directories(z3_git::z3_git INTERFACE ${install_dir}/include)
-  target_link_libraries(z3_git::z3_git INTERFACE z3)
-  target_link_directories(z3_git::z3_git INTERFACE ${install_dir}/lib)
-  message(STATUS "Use Z3 from ${install_dir}")
-
-  find_package(OpenMP)
-
-  if(OPENMP_FOUND)
-    message(STATUS "Use Z3 with OpenMP")
-  else()
-    message(STATUS "Use Z3 without OpenMP")
-  endif()
+if(NOT riscvfw_POPULATED)
+    FetchContent_Populate(z3_git)
 endif()
+set(install_dir ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
+    set(install_dir ${CMAKE_BINARY_DIR}/solvers/z3)
+endif()
+
+find_package(Threads REQUIRED)
+find_package(OpenMP)
+
+if(OPENMP_FOUND)
+  message(STATUS "Use Z3 with OpenMP")
+else()
+  set(Z3_OPENMP --noomp)
+  message(STATUS "Use Z3 without OpenMP")
+endif()
+
+add_custom_command(
+  OUTPUT ${install_dir}/lib/libz3.a
+  WORKING_DIRECTORY ${z3_git_SOURCE_DIR}
+  COMMAND git reset --hard HEAD
+  COMMAND git apply -p0 ${CMAKE_CURRENT_LIST_DIR}/z3-z3-4.6.0__permutation_matrix.patch
+  COMMAND ./configure --staticlib ${Z3_OPENMP} --prefix=${install_dir}
+  COMMAND make -C build -j
+  COMMAND make -C build install
+  COMMENT "Building Z3 library"
+  USES_TERMINAL
+)
+add_custom_target(z3_git DEPENDS ${install_dir}/lib/libz3.a)
+add_library(z3 INTERFACE)
+target_include_directories(z3 INTERFACE ${install_dir}/include)
+target_link_libraries(z3 INTERFACE ${install_dir}/lib/libz3.a)
+if(OPENMP_FOUND)
+  target_link_libraries(z3 INTERFACE OpenMP::OpenMP_CXX)
+endif()
+target_link_libraries(z3 INTERFACE Threads::Threads)
+target_link_directories(z3 INTERFACE ${install_dir}/lib)
+add_dependencies(z3 DEPENDS z3_git)
+add_library(z3::z3 ALIAS z3)
+
+message(STATUS "Use Z3 from ${install_dir}")
+
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+set(z3_CMAKE_CONFIG_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/z3)
+
+install(TARGETS z3 EXPORT z3-targets)
+
+install(
+  EXPORT z3-targets
+  DESTINATION ${z3_CMAKE_CONFIG_DIR}
+)
+
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/z3-config-version.cmake
+    VERSION 4.6.0
+    COMPATIBILITY AnyNewerVersion
+)
+
+configure_package_config_file(
+    ${CMAKE_CURRENT_LIST_DIR}/z3-config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/z3-config.cmake
+    INSTALL_DESTINATION ${z3_CMAKE_CONFIG_DIR}
+)
+
+install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/z3-config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/z3-config-version.cmake
+    DESTINATION ${z3_CMAKE_CONFIG_DIR})
