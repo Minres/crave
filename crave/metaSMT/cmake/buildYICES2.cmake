72f5730533290fb51556a6d1e60a296d77cf253f
stas <stas@minres.com>
2024-02-26 09:44:39 +0100

    cmake export yices2

--- crave/metaSMT/cmake/buildYICES2.cmake
+++ crave/metaSMT/cmake/buildYICES2.cmake
@@ -7,36 +7,58 @@ FetchContent_GetProperties(yices2_repo)
 
 if(NOT yices2_repo_POPULATED)
     FetchContent_Populate(yices2_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/yices2)
-    endif()
-
-    if(NOT yices2_EXTERNAL_PROJECT_ADDED)
-        execute_process(
-            COMMAND mkdir -p ${install_dir}/include
-            COMMAND mkdir -p ${install_dir}/lib
-            WORKING_DIRECTORY ${yices2_repo_SOURCE_DIR}
-        )
-
-        ExternalProject_Add(
-            yices2
-            BUILD_IN_SOURCE TRUE
-            SOURCE_DIR ${yices2_repo_SOURCE_DIR}
-            CONFIGURE_COMMAND autoconf
-            COMMAND ./configure --prefix=${install_dir}
-            BUILD_COMMAND make -j
-            INSTALL_COMMAND make install -j
-        )
-
-        set(yices2_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    add_library(yices2_git::yices2_git INTERFACE IMPORTED)
-    target_include_directories(yices2_git::yices2_git INTERFACE ${install_dir}/include)
-    target_link_libraries(yices2_git::yices2_git INTERFACE yices)
-    target_link_directories(yices2_git::yices2_git INTERFACE ${install_dir}/lib)
-    message(STATUS "Use yices2 from ${install_dir}")
 endif()
+
+set(install_dir ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
+    set(install_dir ${CMAKE_BINARY_DIR}/solvers/yices2)
+endif()
+
+add_custom_command(
+    OUTPUT ${install_dir}/lib/libyices.a
+    WORKING_DIRECTORY ${yices2_repo_SOURCE_DIR}
+    COMMAND autoconf
+    COMMAND ./configure --prefix=${install_dir}
+    COMMAND make -j
+    COMMAND make install -j
+    COMMENT "Building Yicies library"
+    USES_TERMINAL
+)
+
+add_custom_target(yices2_repo DEPENDS ${install_dir}/lib/libyices.a)
+add_library(yices2 INTERFACE)
+target_include_directories(yices2 INTERFACE ${install_dir}/include)
+target_link_libraries(yices2 INTERFACE ${install_dir}/lib/libyices.a gmp)
+target_link_directories(yices2 INTERFACE ${install_dir}/lib)
+add_dependencies(yices2 DEPENDS yices2_repo)
+add_library(yices2::yices2 ALIAS yices2)
+# install the target
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+set(yices2_CMAKE_CONFIG_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/yices2)
+
+install(TARGETS yices2 EXPORT yices2-targets)
+
+install(
+  EXPORT yices2-targets
+  DESTINATION ${yices2_CMAKE_CONFIG_DIR}
+)
+
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/yices2-config-version.cmake
+    VERSION 2.6.4
+    COMPATIBILITY AnyNewerVersion
+)
+
+configure_package_config_file(
+    ${CMAKE_CURRENT_LIST_DIR}/yices2-config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/yices2-config.cmake
+    INSTALL_DESTINATION ${yices2_CMAKE_CONFIG_DIR}
+)
+
+install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/yices2-config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/yices2-config-version.cmake
+    DESTINATION ${yices2_CMAKE_CONFIG_DIR})
+message(STATUS "Use yices2 from ${install_dir}")
