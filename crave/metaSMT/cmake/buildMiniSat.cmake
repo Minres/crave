79379991cc932ff98e08283f4fafed71a81eb429
stas <stas@minres.com>
2024-02-26 16:13:55 +0100

    cmake export minisat and STP

--- crave/metaSMT/cmake/buildMiniSat.cmake
+++ crave/metaSMT/cmake/buildMiniSat.cmake
@@ -1,44 +1,32 @@
-if(NOT DEFINED MINISAT_MODULE_INCLUDED)
-    set(MINISAT_MODULE_INCLUDED TRUE INTERNAL "Flag indicating whether Minisat module has already been included")
-
-    FetchContent_Declare(
-        minisat_repo
-        GIT_REPOSITORY https://github.com/stp/minisat.git
-        GIT_TAG releases/2.2.1
-    )
-    FetchContent_GetProperties(minisat_repo)
+FetchContent_Declare(
+    minisat_repo
+    GIT_REPOSITORY https://github.com/stp/minisat.git
+    GIT_TAG 14c78206cd12d1d36b7e042fa758747c135670a4
+)
+FetchContent_GetProperties(minisat_repo)
+if(NOT minisat_repo_POPULATED)
     FetchContent_Populate(minisat_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/minisat)
-    endif()
-    execute_process(
-        COMMAND mkdir -p ${install_dir}/include
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    )
-
-    if(NOT minisat_EXTERNAL_PROJECT_ADDED)
-        ExternalProject_Add(
-            minisat
-            SOURCE_DIR ${minisat_repo_SOURCE_DIR}
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${install_dir} 
-        )
-        set(minisat_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    # Manually set Minisat include and library directories
-    set(MINISAT_INCLUDE_DIRS ${install_dir}/include CACHE INTERNAL "Minisat include directories")
-    set(MINISAT_LIBRARY_DIRS ${install_dir}/lib CACHE INTERNAL "Minisat library directories")
-
-    add_library(minisat_git::minisat_git INTERFACE IMPORTED)
-    target_include_directories(minisat_git::minisat_git INTERFACE ${MINISAT_INCLUDE_DIRS})
-    target_link_libraries(minisat_git::minisat_git INTERFACE minisat)
-    target_link_directories(minisat_git::minisat_git INTERFACE ${MINISAT_LIBRARY_DIRS})
-    target_compile_definitions(minisat_git::minisat_git INTERFACE
-        __STDC_LIMIT_MACROS
-        __STDC_FORMAT_MACROS
-    )
-    message(STATUS "Use minisat from ${install_dir}")
 endif()
+
+set(install_dir ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
+    set(install_dir ${CMAKE_BINARY_DIR}/solvers/minisat)
+endif()
+
+# minisat installation is required by STP in configure phase
+execute_process(
+  WORKING_DIRECTORY ${minisat_repo_SOURCE_DIR}
+  COMMAND bash -c "cmake -B build -DCMAKE_INSTALL_PREFIX=${install_dir} . && make -C build install -j"
+  RESULT_VARIABLE minisat_RESULT
+  ECHO_OUTPUT_VARIABLE
+  ECHO_ERROR_VARIABLE
+)
+
+if(NOT minisat_RESULT EQUAL 0)
+  message(FATAL_ERROR "Failed to build minisat: ${minisat_OUTPUT}")
+endif()
+
+find_package(minisat CONFIG REQUIRED PATHS ${install_dir}/lib/cmake)
+
+message(STATUS "Use minisat from ${install_dir}")
--- crave/metaSMT/cmake/buildSTP.cmake
+++ crave/metaSMT/cmake/buildSTP.cmake
@@ -1,5 +1,3 @@
-include(cmake/buildMiniSat.cmake)
-
 FetchContent_Declare(
     stp_repo
     GIT_REPOSITORY https://github.com/stp/stp.git
@@ -9,33 +7,13 @@ FetchContent_GetProperties(stp_repo)
 
 if(NOT stp_repo_POPULATED)
     FetchContent_Populate(stp_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/stp)
-    endif()
-
-    if(NOT stp_EXTERNAL_PROJECT_ADDED)
-        execute_process(
-            COMMAND mkdir -p ${install_dir}/include
-            WORKING_DIRECTORY ${stp_repo_SOURCE_DIR}
-        )
-
-        ExternalProject_Add(
-            stp
-            BUILD_IN_SOURCE TRUE
-            SOURCE_DIR ${stp_repo_SOURCE_DIR}
-            CMAKE_ARGS -DENABLE_PYTHON_INTERFACE=off -DONLY_SIMPLE=on -DNOCRYPTOMINISAT=on -DCMAKE_INSTALL_PREFIX=${install_dir} -DMINISAT_INCLUDE_DIR=${MINISAT_INCLUDE_DIRS} -DMINISAT_LIBRARY=${MINISAT_LIBRARY_DIRS}/libminisat.so 
-            DEPENDS minisat_git::minisat_git
-        )
-
-        set(stp_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    add_library(stp_git::stp_git INTERFACE IMPORTED)
-    target_include_directories(stp_git::stp_git INTERFACE ${install_dir}/include)
-    target_link_libraries(stp_git::stp_git INTERFACE stp)
-    target_link_directories(stp_git::stp_git INTERFACE ${install_dir}/lib64)
-    message(STATUS "Use STP from ${install_dir}")
 endif()
+
+option(ENABLE_PYTHON_INTERFACE OFF)
+option(ONLY_SIMPLE ON)
+option(NOCRYPTOMINISAT ON)
+add_subdirectory(${stp_repo_SOURCE_DIR} ${CMAKE_BINARY_DIR}/stp)
+target_include_directories(stp PUBLIC 
+  $<BUILD_INTERFACE:${stp_repo_SOURCE_DIR}/include>
+  $<INSTALL_INTERFACE:include>
+)
