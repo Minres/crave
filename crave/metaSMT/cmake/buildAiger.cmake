420ce2e49c60de81e1a4b6997da472988767338e
stas <stas@minres.com>
2024-02-26 12:20:14 +0100

    cmake export aiger and picosat

--- crave/metaSMT/cmake/buildAiger.cmake
+++ crave/metaSMT/cmake/buildAiger.cmake
@@ -6,33 +6,12 @@ FetchContent_GetProperties(aiger_repo)
 
 if(NOT aiger_repo_POPULATED)
     FetchContent_Populate(aiger_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/aiger)
-    endif()
-
-    if(NOT aiger_EXTERNAL_PROJECT_ADDED)
-        execute_process(
-            COMMAND mkdir -p ${install_dir}/include
-            COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/AigerCMakeLists.txt CMakeLists.txt
-            WORKING_DIRECTORY ${aiger_repo_SOURCE_DIR}
-        )
-
-        ExternalProject_Add(
-            aiger
-            BUILD_IN_SOURCE TRUE
-            SOURCE_DIR ${aiger_repo_SOURCE_DIR}
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${install_dir}
-        )
-
-        set(aiger_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    add_library(aiger_git::aiger_git INTERFACE IMPORTED)
-    target_include_directories(aiger_git::aiger_git INTERFACE ${install_dir}/include)
-    target_link_libraries(aiger_git::aiger_git INTERFACE Aiger)
-    target_link_directories(aiger_git::aiger_git INTERFACE ${install_dir}/lib)
-    message(STATUS "Use aiger from ${install_dir}")
 endif()
+
+execute_process(
+    COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/AigerCMakeLists.txt CMakeLists.txt
+    COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/aiger-config.cmake.in .
+    WORKING_DIRECTORY ${aiger_repo_SOURCE_DIR}
+)
+
+add_subdirectory(${aiger_repo_SOURCE_DIR} ${CMAKE_BINARY_DIR}/aiger)
--- crave/metaSMT/cmake/buildPicoSat.cmake
+++ crave/metaSMT/cmake/buildPicoSat.cmake
@@ -6,33 +6,12 @@ FetchContent_GetProperties(picosat_repo)
 
 if(NOT picosat_repo_POPULATED)
     FetchContent_Populate(picosat_repo)
-
-    set(install_dir ${CMAKE_INSTALL_PREFIX})
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        # Fallback in case where CMAKE_INSTALL_PREFIX is not explicitly set by the user
-        set(install_dir ${CMAKE_BINARY_DIR}/solvers/picosat)
-    endif()
-
-    if(NOT picosat_EXTERNAL_PROJECT_ADDED)
-        execute_process(
-            COMMAND mkdir -p ${install_dir}/include
-            COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/PicosatCMakeLists.txt CMakeLists.txt
-            WORKING_DIRECTORY ${picosat_repo_SOURCE_DIR}
-        )
-
-        ExternalProject_Add(
-            picosat
-            BUILD_IN_SOURCE TRUE
-            SOURCE_DIR ${picosat_repo_SOURCE_DIR}
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${install_dir}
-        )
-
-        set(picosat_EXTERNAL_PROJECT_ADDED TRUE CACHE INTERNAL "Flag indicating whether the external project has been added")
-    endif()
-
-    add_library(picosat_git::picosat_git INTERFACE IMPORTED)
-    target_include_directories(picosat_git::picosat_git INTERFACE ${install_dir}/include)
-    target_link_libraries(picosat_git::picosat_git INTERFACE picosat)
-    target_link_directories(picosat_git::picosat_git INTERFACE ${install_dir}/lib)
-    message(STATUS "Use picosat from ${install_dir}")
 endif()
+
+execute_process(
+    COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/PicosatCMakeLists.txt CMakeLists.txt
+    COMMAND cp ${CMAKE_CURRENT_LIST_DIR}/picosat-config.cmake.in .
+    WORKING_DIRECTORY ${picosat_repo_SOURCE_DIR}
+)
+
+add_subdirectory(${picosat_repo_SOURCE_DIR} ${CMAKE_BINARY_DIR}/picosat)
