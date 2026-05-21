21ddddb9c3371a1487e04d2f5a59a126b050c149
stas <stas@minres.com>
2026-05-21 14:50:59 +0200

    Offline support for PicoSat and Lingeling

--- crave/metaSMT/cmake/buildLingeling.cmake
+++ crave/metaSMT/cmake/buildLingeling.cmake
@@ -1,6 +1,13 @@
+set(LINGELING_SOURCE_ARGS
+    URL https://fmv.jku.at/lingeling/lingeling-ayv-86bf266-140429.zip
+)
+
+# Resolve local source if in offline mode
+metasmt_resolve_local_source(lingeling LINGELING_SOURCE_ARGS)
+
 FetchContent_Declare(
     lingeling_repo
-    URL https://fmv.jku.at/lingeling/lingeling-ayv-86bf266-140429.zip
+    ${LINGELING_SOURCE_ARGS}
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 FetchContent_GetProperties(lingeling_repo)
@@ -15,6 +22,16 @@ execute_process(
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/lglcflags.h.in.cmake .
     WORKING_DIRECTORY ${lingeling_repo_SOURCE_DIR}
 )
+
+if(NOT METASMT_DEPS_DIR)
+    # Stage the populated Lingeling source tree directly because it does not use ExternalProject.
+    add_custom_command(TARGET metasmt-export-deps-stage POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${METASMT_EXPORT_STAGING_DIR}/lingeling"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${lingeling_repo_SOURCE_DIR}" "${METASMT_EXPORT_STAGING_DIR}/lingeling"
+        COMMAND ${CMAKE_COMMAND} -E remove_directory "${METASMT_EXPORT_STAGING_DIR}/lingeling/.git"
+        COMMENT "Staging lingeling source for export from ${lingeling_repo_SOURCE_DIR}"
+    )
+endif()
 
 add_subdirectory(${lingeling_repo_SOURCE_DIR} ${CMAKE_BINARY_DIR}/lingeling)
 
--- crave/metaSMT/cmake/buildPicoSat.cmake
+++ crave/metaSMT/cmake/buildPicoSat.cmake
@@ -1,6 +1,13 @@
+set(PICOSAT_SOURCE_ARGS
+    URL https://fmv.jku.at/picosat/picosat-965.tar.gz
+)
+
+# Resolve local source if in offline mode
+metasmt_resolve_local_source(picosat PICOSAT_SOURCE_ARGS)
+
 FetchContent_Declare(
     picosat_repo
-    URL https://fmv.jku.at/picosat/picosat-965.tar.gz
+    ${PICOSAT_SOURCE_ARGS}
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 FetchContent_GetProperties(picosat_repo)
@@ -13,6 +20,16 @@ execute_process(
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/PicosatCMakeLists.txt CMakeLists.txt
     WORKING_DIRECTORY ${picosat_repo_SOURCE_DIR}
 )
+
+if(NOT METASMT_DEPS_DIR)
+    # Stage the populated PicoSAT source tree directly because it does not use ExternalProject.
+    add_custom_command(TARGET metasmt-export-deps-stage POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${METASMT_EXPORT_STAGING_DIR}/picosat"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${picosat_repo_SOURCE_DIR}" "${METASMT_EXPORT_STAGING_DIR}/picosat"
+        COMMAND ${CMAKE_COMMAND} -E remove_directory "${METASMT_EXPORT_STAGING_DIR}/picosat/.git"
+        COMMENT "Staging picosat source for export from ${picosat_repo_SOURCE_DIR}"
+    )
+endif()
 
 add_subdirectory(${picosat_repo_SOURCE_DIR} ${CMAKE_BINARY_DIR}/picosat)
 
